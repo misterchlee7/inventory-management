b8e55d2d919f05618639647eab4c8a2aae951a7e
Chang Lee <cleeucla@gmail.com>
2018-12-04 15:44:13 -0800

    error404 page

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Login from '../../components/Login/Login';
+import Error404 from '../../components/Error404/Error404';
 
 class Layout extends Component {
   render() {
@@ -9,6 +10,7 @@ class Layout extends Component {
       <Router>
         <Switch>
           <Route exact path ='/' component={Login} />
+          <Route component={Error404} />
         </Switch>
       </Router>
     );
